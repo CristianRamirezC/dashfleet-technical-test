206f2f05459ced8782285b2b489704e9b511999b
Camilo Ramirez <crc97cristian@gmail.com>
2023-07-05 15:47:39 -0500

    agregando build types, flavors y resource folders para los variants

---
+++ app/src/debug/java/com/example/dashfleet-technical-test/DemoClass.kt
@@ -0,0 +1,4 @@
+package com.example.`dashfleet-technical-test`
+
+class DemoClass {
+}
--- app/src/main/java/com/example/dashfleet_technical_test/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/dashfleet_technical_test/ui/view/MainActivity.kt
@@ -4,28 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.dashfleet_technical_test.domain.model.userRoutes.UserRoutes
-import com.example.dashfleet_technical_test.ui.theme.DashfleetTechnicalTestTheme
-import com.example.dashfleet_technical_test.ui.view.bottomNavigation.MainNavigationScreenView
-import com.example.dashfleet_technical_test.ui.view.loginScreen.LoginScreen
 import com.example.dashfleet_technical_test.ui.viewModel.user.UserLoginViewModel
 import com.example.dashfleet_technical_test.ui.viewModel.userRoutes.UserRoutesViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,6 +19,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainView(userLoginViewModel, userRoutesViewModel)
         }
+        //validations with build types/flavors
+
+//        if(BuildConfig.BUILD_TYPE == "release") {
+//            //Do something
+//        }
+//
+//        val endpointURL = BuildConfig.URL
+//        if (BuildConfig.FLAVOR == "premium") {
+//            //Do something for premium users
+//
+//        }
     }
 }
 
